e87720b1b2ababf17fc3f0812a31f2cb1b726019
jessica-dds <jessica.gleizer@hotmail.com>
2023-11-29 21:25:26 -0300

    Exercícios objetos

--- intro.js
+++ intro.js
@@ -2,6 +2,8 @@ const pessoa = {
     nome: "Jéssica",
     altura: 1.62,
     peso: 76
-};
-
+}
 console.log(`Meu nome é ${pessoa.nome}, tenho ${pessoa.altura}m e peso ${pessoa.peso}kg.`);
+
+// notação de cochetes
+// pessoa["nome"] = Jéssica;
