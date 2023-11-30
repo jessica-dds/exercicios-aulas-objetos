f562e4b4d3dd3b8384b79534c2d734ac5a852a7f
jessica-dds <jessica.gleizer@hotmail.com>
2023-11-30 18:18:32 -0300

    Exercício shorthand

---
+++ exercicio-shorthand.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    nome: "Jéssica",
+    altura: 1.62,
+    peso: 76,
+    carro: {
+        marca: "Toyota",
+        modelo: "Etios",
+        ano: 2020,
+        potencia: 80
+    }
+};
+
+
+console.log(pessoa.carro.marca);
