88f20036c83438c391173a412153660c4b69652a
jessica-dds <jessica.gleizer@hotmail.com>
2023-12-01 23:21:08 -0300

    Exercício 2

---
+++ exercicio2.js
@@ -0,0 +1,28 @@
+
+const consumo = [
+    {
+        nome: "Hamburguer",
+        precoUnitario: 2500,
+        quantidade: 1
+    },
+    {
+        nome: "Suco de laranja",
+        precoUnitario: 1000,
+        quantidade: 2
+    },
+    {
+        nome: "sorvete",
+        precoUnitario: 1500,
+        quantidade: 1
+    }
+]
+
+
+const cartao = {
+    nome: "Arnaldo",
+    idade: 40,
+    consumo
+};
+
+console.log(cartao);
+
